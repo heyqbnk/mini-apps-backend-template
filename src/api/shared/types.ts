062070f9c4f408803b230e4df74c84f7340af224
Vladislav Kibenko <wolframdeus@gmail.com>
2021-09-09 00:49:15 +0500

    refactor(типы): удалить неиспользуемые типы

--- src/api/shared/types.ts
+++ src/api/shared/types.ts
@@ -1,6 +1,5 @@
 import {IVerifiedLaunchParams} from '~/shared/utils';
 import {ParsedUrlQuery} from 'querystring';
-import {Transaction} from '@sentry/types';
 
 export interface IAuthorizedLocals {
   /**
@@ -11,11 +10,4 @@ export interface IAuthorizedLocals {
    * Параметры запуска в виде Query.
    */
   launchParamsQuery: ParsedUrlQuery;
-}
-
-export interface ISentryFilledLocals {
-  /**
-   * Транзакция сентри.
-   */
-  transaction: Transaction;
 }
--- src/api/shared/utils.ts
+++ src/api/shared/utils.ts
@@ -1,4 +1,4 @@
-import {IAuthorizedLocals, ISentryFilledLocals} from '~/api/shared/types';
+import {IAuthorizedLocals} from '~/api/shared/types';
 
 /**
  * Утверждает, что переданное значение содержит информацию о параметрах
@@ -9,15 +9,4 @@ export function isAuthorizedLocals(
   value: Record<string, any>,
 ): value is IAuthorizedLocals {
   return 'launchParams' in value && 'launchParamsQuery' in value;
-}
-
-/**
- * Утверждает, что переданное значение содержит информацию, которую вносит
- * обработчик Sentry.
- * @param value
- */
-export function isSentryFilledLocals(
-  value: Record<string, any>,
-): value is ISentryFilledLocals {
-  return 'transaction' in value;
 }
